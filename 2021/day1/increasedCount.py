9b9b5f472aa0a88f41d25ab4ec7f5d4469026f84
Andres Garcia <andresf212000@gmail.com>
2021-12-01 13:09:52 -0500

    Day 1 Part 2

--- 2021/day1/increasedCount.py
+++ 2021/day1/increasedCount.py
@@ -4,6 +4,7 @@ import pathlib
 
 
 def partOne():
+    """ Compare a list of numbers and count how many are bigger than the previous """
     filePath = pathlib.Path(__file__).parent.resolve()
     lastNum = 0
     increaseCount = 0
@@ -15,8 +16,42 @@ def partOne():
                 if currNum > lastNum:
                     increaseCount += 1
             lastNum = currNum
-    print(increaseCount)
+    return increaseCount
+
+
+def sumOfArray(givenArray):
+    sum = 0
+    for i in givenArray:
+        sum += i
+    return sum
+
+
+def partTwo():
+    """ Compare measurement totals and count how many are bigger than the previous """
+    filePath = pathlib.Path(__file__).parent.resolve()
+    increaseCount = 0
+
+    with open(f'{filePath}/input.txt') as f:
+        lines = f.readlines()
+        index = 0
+        lastSum = 0
+        for line in lines:
+            try:
+                currArray = [int(line), int(
+                    lines[index+1]), int(lines[index+2])]
+                if lastSum == 0:
+                    lastSum = sumOfArray(currArray)
+                else:
+                    currSum = sumOfArray(currArray)
+                    if currSum > lastSum:
+                        increaseCount += 1
+                    lastSum = currSum
+                index += 1
+            except IndexError:
+                continue
+    return increaseCount
 
 
 if __name__ == "__main__":
-    partOne()
+    print(f"partOne: {partOne()}")
+    print(f"partTwo: {partTwo()}")
