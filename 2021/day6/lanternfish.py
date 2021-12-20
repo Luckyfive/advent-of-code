e4d749b20bc0ebdede70086457c4a471e9c1269b
Andres Garcia <andresf212000@gmail.com>
2021-12-20 00:02:54 -0500

    refactored logic to account for fish as buckets

--- 2021/day6/lanternfish.py
+++ 2021/day6/lanternfish.py
@@ -6,42 +6,65 @@ import math
 def partOne():
     """ Given certain conditions, return how many lanternfish are there after 80 days."""
     filePath = pathlib.Path(__file__).parent.resolve()
-    counts = []
-    finalCount = 0
+    finalCounts = [0] * 9;
+
     with open(f'{filePath}/input.txt') as f:
         lines = f.readlines()
         counts = [int(num) for num in lines[0].split(',')]
 
-        print(f"Initial State: {counts}")
+        for fish in counts:
+            finalCounts[fish] += 1
+
         for day in range(1, 81):
-            for index, currFish in enumerate(counts):
-                if currFish == 0:
-                    counts.append(9)
-                    counts[index] = 6
-                    continue
-                counts[index] = currFish-1
-    return len(counts)
+            spawned = 0
+
+            for index, fish in enumerate(finalCounts):
+                if index == 0:
+                    spawned = finalCounts[0]
+                else:
+                    finalCounts[index-1] = finalCounts[index]
+                
+                finalCounts[index] = 0
+            
+            finalCounts[6] += spawned
+            finalCounts[8] = spawned
+
+        result = 0
+        for currCount in finalCounts:
+            result += currCount
+    return result
 
 
 def partTwo():
     """ Given certain conditions, return how many lanternfish are there after 256 days."""
     filePath = pathlib.Path(__file__).parent.resolve()
-    counts = []
-    finalCount = 0
+    finalCounts = [0] * 9;
+
     with open(f'{filePath}/input.txt') as f:
         lines = f.readlines()
         counts = [int(num) for num in lines[0].split(',')]
 
-        # print(f"Initial State: {counts}")
-        for day in range(1, 257):
-            for index, currFish in enumerate(counts):
-                if currFish == 0:
-                    counts.append(9)
-                    counts[index] = 6
-                    continue
-                counts[index] = currFish-1
-    return len(counts)
+        for fish in counts:
+            finalCounts[fish] += 1
 
+        for day in range(1, 257):
+            spawned = 0
+
+            for index, fish in enumerate(finalCounts):
+                if index == 0:
+                    spawned = finalCounts[0]
+                else:
+                    finalCounts[index-1] = finalCounts[index]
+                
+                finalCounts[index] = 0
+            
+            finalCounts[6] += spawned
+            finalCounts[8] = spawned
+
+        result = 0
+        for currCount in finalCounts:
+            result += currCount
+    return result
 
 if __name__ == "__main__":
     print(f"partOne: {partOne()}")
